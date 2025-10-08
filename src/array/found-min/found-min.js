9d57f5637b991bcff27c01fec136fe9403a21783
ClarissaCardarelli <ccardare@gmail.com>
2025-10-08 09:52:32 +0200

    worked on array found and array inverse but did not test yet

--- src/array/found-min/found-min.js
+++ src/array/found-min/found-min.js
@@ -14,4 +14,21 @@ You can't use the function Math.min()
 
 // TODO add your code here
 
+// function min(array) {
+//   if (!Array.isArray(array) || array.length === 0) return null;
+//   let newArray = [...array].sort((a, b) => a - b);
+//   return newArray[0];
+// }
+
+// check if array, use Array.isArray(tester)
+// spread to create copy. we do not want to change the og
+
+// could do if array, do this, else...
+
+function min(array) {
+  return Array.isArray(array) && array.length > 0
+    ? [...array].sort((a, b) => a - b)[0]
+    : null;
+}
+
 module.exports = min;
--- src/array/inverse-values/inverse-values.js
+++ src/array/inverse-values/inverse-values.js
@@ -16,4 +16,42 @@ Don't mutate the parameter.
 
 // TODO add your code here
 
+// let newArray = [];
+
+// function inverse(array) {
+//   if (!Array.isArray(array) || array.length === 0) return newArray;
+
+//   for (let i = 0; i < array.length; i++) {
+// if (array[i] <= 0) {
+//   newArray.push(Math.abs(array[i]));
+// } else {
+//   newArray.push(-Math.abs(array[i]));
+// }
+
+// array[i] <= 0
+//   ? newArray.push(Math.abs(array[i]))
+//   : newArray.push(-Math.abs(array[i]));
+//   }
+//   return newArray;
+// }
+
+//doing the same thing (push) so can simply with map
+
+// function inverse(array) {
+//   if (!Array.isArray(array) || array.length === 0) return [];
+
+// return array.map(num => -num)
+// dont like that it changes 0 to -0
+
+//   return array.map((num) => (num <= 0 ? Math.abs(num) : -Math.abs(num)));
+// }
+
+// in above, we are doing if true, return this, else... so the whole thing could be a ternary operator : is array, do this, if not, return empty array. But less readable ?
+
+function inverse(array) {
+  return Array.isArray(array) && array.length > 0
+    ? array.map((num) => (num <= 0 ? Math.abs(num) : -num))
+    : [];
+}
+
 module.exports = inverse;
